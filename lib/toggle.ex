2762ef842668245cad0550258232e9b6900a83e1
Chris <clwilson4@cougars.ccis.edu>
2022-03-01 21:27:38 -0600

    added tests and docs for toggle

--- lib/toggle.ex
+++ lib/toggle.ex
@@ -1,16 +1,39 @@
 defmodule Toggle do
   use Agent
 
+  @moduledoc """
+    Toggles enabled/disabled for feed
+  """
+
+  @doc false
+  @spec start_link(any) :: {:error, any} | {:ok, pid}
   def start_link(initial_value \\ :enabled) do
     Agent.start_link(fn -> initial_value end, name: __MODULE__)
   end
 
+
+  @doc """
+
+    Returns the current toggle value
+
+  """
+  @spec toggle_value :: :enabled | :disabled
   def toggle_value do
     Agent.get(__MODULE__, & &1)
   end
 
+  @doc """
+
+    Toggles current toggle value
+
+      {:ok, pid} = Toggle.start_link(:enabled)
+      Toggle.toggle_value
+      :disabled
+
+  """
+  @spec toggle_toggle :: :enabled | :disabled
   def toggle_toggle do
-    Agent.update(__MODULE__, &(uno_reverse(&1)))
+    Agent.get_and_update(__MODULE__, &({&1, uno_reverse(&1)}))
   end
 
   defp uno_reverse(:enabled), do: :disabled
---
+++ test/exdoc_test.exs
@@ -0,0 +1,4 @@
+defmodule ExdocTest do
+  use ExUnit.Case, async: true
+  doctest Toggle
+end
