429c81ed8d2ecbc4cfe7cfa9e259bebaf0094f1b
Chris <clwilson4@cougars.ccis.edu>
2022-03-01 21:37:52 -0600

    added poolboy to example

--- mix.exs
+++ mix.exs
@@ -24,7 +24,8 @@ defmodule Algolia.MixProject do
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
       {:remix, "~> 0.0.2", only: :dev},
-      {:ex_doc, "~> 0.28.2", only: :dev, runtime: false}
+      {:ex_doc, "~> 0.28.2", only: :dev, runtime: false},
+      {:poolboy, "~> 1.5"}
     ]
   end
 end
