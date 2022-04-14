fc6e75ab7f498c890167312c8ab3dcda6dd44d1f
Chris <clwilson4@cougars.ccis.edu>
2022-04-13 20:40:27 -0500

    added barebones supervisors

--- lib/event/event_sup.ex
+++ lib/event/event_sup.ex
@@ -1,5 +1,16 @@
 defmodule EventSupervisor do
+  use Supervisor
 
+  def init(_args) do
 
-  
+    children = [
+      {Alarm, []},
+      {EventLogger, []}
+    ]
+
+    opts = [strategy: :one_for_one, name: __MODULE__]
+
+    Supervisor.start_link(children, opts)
+  end
+
 end
--- lib/freq/frequency_sup.ex
+++ lib/freq/frequency_sup.ex
@@ -1,4 +1,15 @@
 defmodule FrequencySupervisor do
+  use Supervisor
 
-  
+  def init(_args) do
+    children = [
+      {FrequencyWorker, [] },
+      {FrequencyOverload, []}
+    ]
+
+    opts = [ strategy: :one_for_one name: __MODULE__]
+
+    Supervisor.start_link(children, opts)
+  end
+
 end
